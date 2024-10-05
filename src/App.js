71f07eb0462eaa76803af9e1dc2f68d8adce2781
Karlina <107602124+karlinamaya@users.noreply.github.com>
2024-10-04 23:13:09 -0400

    updated title!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Knight's Wildlife Tracker
+          Knight's Wildlife Tracker!
         </p>
         <a
           className="App-link"

9b71dbade2d03b746e127703c601baa99f46386c
Karlina <107602124+karlinamaya@users.noreply.github.com>
2024-10-04 23:08:56 -0400

    Updated title

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Knight's Wildlife Tracker
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn
-        </a>
       </header>
     </div>
   );

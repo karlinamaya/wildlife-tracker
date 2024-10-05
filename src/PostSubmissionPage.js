16f03e96238aa356b274be7a0d4b54f701086157
Amani Hassan <amanihas2021@gmail.com>
2024-10-05 13:49:22 -0400

    postSubmission

--- src/PostSubmissionPage.js
+++ src/PostSubmissionPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PostForm from './PostForm';
+import './PostSubmissionPage.css'; // Ensure this path is correct
 
 function PostSubmissionPage({ fetchPosts }) {
   return (
-    <div>
+    <div className="post-submission-page">
       <h1>Upload a Post</h1>
       <PostForm fetchPosts={fetchPosts} />
     </div>
